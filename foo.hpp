3ab978808248f8fee4b087790b04b80f5bbb047d
Łukasz Rybakowski <rybakowski2001@gmail.com>
2024-11-24 20:08:56 +0100

    modified:   foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -5,8 +5,23 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    // Twoja implementacja tutaj
-    return {};
+std::vector<char> foo(std::list<Human>& people) {
+    std::vector<char> result;
+
+    // Iterujemy po liście osób
+    for (auto& person : people) {
+        // Zwiększamy wiek każdej osoby
+        person.birthday();
+
+        // Sprawdzamy, czy osoba jest "potworem" (nie lubi psów ani kotów)
+        result.push_back(person.isMonster() ? 'n' : 'y');
+    }
+
+    // Odwracamy kolejność w wyniku ręcznie
+    std::vector<char> reversed_result;
+    for (int i = result.size() - 1; i >= 0; --i) {
+        reversed_result.push_back(result[i]);
+    }
+
+    return reversed_result;
 }
